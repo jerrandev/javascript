61599cf611ecde97bb553591bce6493a7a9711f9
Jerran Viana <116609753+jerrandev@users.noreply.github.com>
2023-05-01 21:44:30 -0300

    novo js

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 62
+if (vel > 60) {
+    console.log (`${vel}, Você ultrapassou a velocidade permitida. MULTADO`)
+}
+else {
+    console.log(`${vel}`)
+}
+console.log('Dirija sempre usando o cinto de segurança.')
